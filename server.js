33c45b42fdc57fac9c0d4f19b5505b5ec332fb7c
Isaac Joshua <isaacjcarnes@gmail.com>
2021-12-06 20:55:37 -0500

    Added utils, views and layouts folder. Added helpers.js. Added main.handlebars and savedDog.handlebars. Edited server.js to implement changes.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,6 +23,11 @@ const sess = {
 };
   
 app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
