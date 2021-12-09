374258d467c5e854c578468b8238edec75084c09
Isaac Joshua <isaacjcarnes@gmail.com>
2021-12-08 19:53:56 -0500

    Request from dog.ceo API works

--- controllers/api/dogsRoutes.js
+++ controllers/api/dogsRoutes.js
@@ -1,3 +1,46 @@
 const router = require('express').Router();
+const https = require('https');
+
+const getBreedList = async() => {
+    let url = 'https://dog.ceo/api/breeds/list/all';
+    https.get(url, (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data));
+        });
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
+}
+/*function getBreedList(){
+    let https = require('https');
+    let url = 'https://dog.ceo/api/breeds/list/all';
+
+    https.get(url, (response) => {
+        body = '';
+        response.on('data', function(chunk) {
+            body += chunk
+        });
+        console.log(body);
+        return body;
+    });
+};*/
+
+router.get('/', async (req, res) => {
+    try{
+        let breedList = getBreedList();
+        res.json(breedList);
+    } catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
--- controllers/api/petfinderRoutes.js
+++ controllers/api/petfinderRoutes.js
@@ -2,4 +2,34 @@
 //shVnvnU2ZtPaUcIEAsW6YYpmvykYapVcej1M51GM
 const router = require('express').Router();
 
+//const fetch = require("node-fetch");
+
+
+/*function getToken(){
+    var url = 'https://api.petfinder.com/v2/oauth2/token'
+    var id = 'bdQfKZtELZCIkwT9oFyeHWcnFNopLlJviUACXCaFPiK5ynBPIW';
+    var secret = 'shVnvnU2ZtPaUcIEAsW6YYpmvykYapVcej1M51GM';
+
+    fetch(url, { 
+    method: 'post', 
+    headers: new Headers({
+        'grant_type': 'client_credentials',
+        'client_id': id,
+        'client_secret': secret,
+    }), 
+  }).then((response) => {
+      console.log(response);
+      return response;
+  });
+}
+
+router.get('/', async (req, res) => {
+    try{
+        let token = getToken();
+    } catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+*/
 module.exports = router;
