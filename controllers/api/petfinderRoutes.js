a75983a9a051479ca1d5eeeccf2dd0bfb81a5f85
Isaac Joshua <isaacjcarnes@gmail.com>
2021-12-11 13:09:16 -0500

    Can now get list of adoptable dogs based on zip code and breed

--- controllers/api/petfinderRoutes.js
+++ controllers/api/petfinderRoutes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const https = require('https');
 
-//const fetch = require("node-fetch");
-
-
 const getToken= () => {
     return new Promise((resolve, reject) => {
         var url = 'https://api.petfinder.com/v2/oauth2/token'
@@ -28,7 +25,6 @@ const getToken= () => {
 
             // The whole response has been received. Print out the result.
             resp.on('end', () => {
-                console.log(data);
                 resolve(data);
             });
         }).on("error", (err) => {
@@ -40,10 +36,67 @@ const getToken= () => {
     });
 }
 
+const getDogsList = async (token, location, breed) => { //Location is based on zip code
+    return new Promise((resolve, reject) => {
+        var accessToken = `Authorization: Bearer ${token.access_token}`;
+        //GET https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}
+        var url = 'https://api.petfinder.com/v2/animals?type=dog';
+        if(location){
+            url = url + '&' + 'location=' +location;
+        }
+        if(breed){
+            url = url + '&' + 'breed=' +breed;
+        }
+
+        var https_options = {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bearer ${token.access_token}`
+            }
+        }
+        const request = https.request(url, https_options, (resp) => {
+            let data = '';
+
+            // A chunk of data has been received.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                resolve(JSON.parse(data));
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            reject(err);
+        });
+        request.end();
+    });
+}
+
+const getDogsFromLocation = async (token, location) =>{
+    let dogList = await getDogsList(token, location, null);
+    return dogList;
+}
+
+const getDogsFromBreed = async (token, breed) =>{
+    let dogList = await getDogsList(token, null, breed);
+    return dogList;
+}
+
+const deconstructDog = (dogArray) => {
+    
+};
+
 router.get('/', async (req, res) => {
     try{
-        let token = await getToken();
-        res.json(token);
+        let token = JSON.parse(await getToken());
+        if(token.access_token != null){
+            let dogList = await getDogsFromLocation(token, "15206");
+            res.json(dogList.animals[0]);
+        } else {
+            res.json(token);
+        }
     } catch (err){
         console.log(err);
         res.status(500).json(err);
