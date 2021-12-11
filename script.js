9f8ff60a19b5047528b8e9439638ec87fd9224c5
Paul McDevitt <paul.mcd1@icloud.com>
2021-12-11 10:09:49 -0500

    remaking sign up modals

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ const Signupbutton = document.getElementById("signup button")
 const Registerbutton = document.getElementById("register button") 
 const Signinbox = document.getElementById("signin")
 const Body = document.getElementById("body")
+const Signupbox = document.getElementById("signup")
 
 Signinbutton.addEventListener("click",InitiateSignIn)
-
-
-
-
-
+Registerbutton.addEventListener("click",function(){
+Closemodal();
+InitiateSignup();
+});
 
 function InitiateSignIn(){
     Signinbox.style.display = "flex";
-    Body.style.opacity = "50%";
-    Signinbox.style.opacity = "100%";
-    
+    Body.style.opacity = "50%"; 
 }
+function Closemodal(){
+Signinbox.style.display ="none";
+Body.style.opacity = "100%";
+}
+
 function InitiateSignup(){
-    
+    Signupbox.style.display = "flex";
+    Body.style.opacity = "0%";
 }
